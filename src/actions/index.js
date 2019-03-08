06675ea242837cb5ff6ed1d67497dfccc499a8fc
raselkarim7 <raselkarim7@gmail.com>
2019-03-08 12:16:47 +0600

    BrowseRouter added insted of HashRouter

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,3 @@
 export const ROOT_URL = 'http://workflow-timeline-backend.local/';
+//export const ROOT_URL = window.location.origin;
+//console.log('ROOT_URL === ', ROOT_URL);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
+//import MainRoute from './routes/MainRoute';
 
 //import 'react-widgets/dist/css/react-widgets.css';
 import styles from './style.css';
@@ -8,9 +9,9 @@ import styles from './style.css';
 import App from './components/App';
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
-  
+  </BrowserRouter>,
+
   document.getElementById('app')
 );
